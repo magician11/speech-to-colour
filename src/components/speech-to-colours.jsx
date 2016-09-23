f9779f9f18f8ad91d1373eee4465ddbb62a0cb37
Andrew Golightly <support@andrewgolightly.com>
2016-09-23 19:08:37 +0800

    completed v1 of app

--- src/components/speech-to-colours.jsx
+++ src/components/speech-to-colours.jsx
@@ -11,18 +11,22 @@ class SpeechToColour extends Component {
       'show me *colour': this.changeColour.bind(this),
     };
 
-    annyang.addCommands(commands);
-
-    this.state = {
-      colour: 'white',
-    };
-
-    annyang.start();
+    if (annyang) {
+      annyang.addCommands(commands);
+      annyang.start();
+      this.state = {
+        colour: 'blue',
+        error: '',
+      };
+    } else {
+      this.state = {
+        error: 'This browser does not support speech recognition. Try using Google Chrome.',
+      };
+    }
   }
 
   changeColour(colourStr) {
     this.setState({ colour: colourStr.replace(/ /g, '') });
-    console.log(colourStr);
   }
 
   render() {
@@ -30,9 +34,21 @@ class SpeechToColour extends Component {
       backgroundColor: this.state.colour,
     };
 
+    let content;
+    if (!this.state.error) {
+      content = (
+        <div className={mainStyles.information}>
+          <p>To see a colour, simply say "show me red" or whatever colour you want to see!</p>
+          <h1>current colour: {this.state.colour}</h1>
+        </div>
+      );
+    } else {
+      content = <h1>{this.state.error}</h1>;
+    }
+
     return (
       <div style={styling} className={mainStyles['speech-to-colour']}>
-        <h1>{this.state.colour}</h1>
+        {content}
       </div>
     );
   }
